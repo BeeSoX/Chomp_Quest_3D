7c729623f7c1a53a22cbe7d74c8726de62ee14f7
Adrien Conrard <adrien.conrard@gmail.com>
2025-02-27 11:36:27 +0100

    tentative 1

--- Assets/LevelingManager.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class LevelingManager : MonoBehaviour
-{
-    public void Openlevel (int levelID) {
-        SceneManager.LoadScene(levelID);
-    }
-}
